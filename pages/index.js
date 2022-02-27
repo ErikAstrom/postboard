1c4f79dc9045fe24629c0db1fde08154137f258c
Erik Åström <erik@stateless.digital>
2022-02-27 16:37:53 +0100

    small fix

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Home = () => {
         )
       ) : (
         <h1 className="text-2xl font-semibold italic">
-          Please sign in to access the postboard
+          Please sign in to access the postboard <3
         </h1>
       )}
     </Layout>
